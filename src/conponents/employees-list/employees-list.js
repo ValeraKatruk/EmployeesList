fc8c2ceff81cf7b70e41e39ff4fe318f6a497178
ValeriiKatruk <Walerik19@mail.ru>
2023-04-28 16:05:08 +0800

    заглушка noEmployees

--- src/conponents/employees-list/employees-list.js
+++ src/conponents/employees-list/employees-list.js
@@ -1,6 +1,18 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item"
+import styled from "styled-components";
+
 import "./employees-list.css"
 
+
+const Ul = styled.ul`
+
+text-align: center;
+height: 150px;
+
+background: url(https://img3.goodfon.ru/original/1920x1080/4/81/tekstura-fon-siniy-oboi.jpg);
+font-size: 24px;
+
+` 
 const EmployeesList = ({ data, onDelete, onToggleProp, onChangeSalary}) => {
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
@@ -20,7 +32,9 @@ const EmployeesList = ({ data, onDelete, onToggleProp, onChangeSalary}) => {
       />
     );
   }); 
-
+ 
+  
+  if(elements.length === 0) return <Ul className="app-list list-group">Нет сотрудников</Ul>
   return <ul className="app-list list-group">{elements}</ul>;
 };
 
